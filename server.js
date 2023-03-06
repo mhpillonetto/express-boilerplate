c3cc21ccc58209bbaa9a65ace502e918a8f330ce
mhpillonetto <marcelohenriquepillonetto@gmail.com>
2023-03-06 15:39:00 -0300

    server running

--- server.js
+++ server.js
@@ -1,4 +1,11 @@
 const express = require('express');
+const app = express();
 const path = require('path');
 const PORT = process.env.PORT || 3500;
 
+app.use('/', (req,res) => {
+    res.send('Hello World!"')
+});
+
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
