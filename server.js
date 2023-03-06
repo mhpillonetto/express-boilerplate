447e3fcdad049b19ff5c74f421ba7ae647d84158
mhpillonetto <marcelohenriquepillonetto@gmail.com>
2023-03-06 15:58:29 -0300

    middlewares and examples

--- server.js
+++ server.js
@@ -1,7 +1,20 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const cors = require('cors');
 const PORT = process.env.PORT || 3500;
+
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json());
+
+app.use(cors());
+
+//custom middleware example - logger
+app.use((req,res,next) => {
+    console.log(`${req.method} ${req.path}`);
+    next();
+})
+
 
 app.get('/', (req,res) => {
     res.send('Hello World!')
