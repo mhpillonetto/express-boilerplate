37e523a2b46e947b97f57404ec566b6c5c198e1e
mhpillonetto <marcelohenriquepillonetto@gmail.com>
2023-03-06 15:51:01 -0300

    middleware example

--- server.js
+++ server.js
@@ -3,9 +3,30 @@ const app = express();
 const path = require('path');
 const PORT = process.env.PORT || 3500;
 
-app.use('/', (req,res) => {
-    res.send('Hello World!"')
+app.get('/', (req,res) => {
+    res.send('Hello World!')
 });
+
+app.get('/users', (req,res) => {
+    res.send('Hello users')
+});
+
+//Middleware example
+const one = (req, res, next) => {
+    console.log('one');
+    next();
+}
+const two = (req, res, next) => {
+    console.log('two');
+    next();
+}
+const three = (req, res) => {
+    console.log('three');
+    res.send('Finished!');
+}
+
+app.get('/chain', [one, two, three]);
+
 
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
